e1f6ffbb9629fea4fd208d505ac5bb2c37d4a837
anshika529 <anshika.negi@gmail.com>
2021-02-19 20:19:22 +0530

    Today's changes with updated code.

--- tests/features/hotsaucemall.feature
+++ tests/features/hotsaucemall.feature
@@ -1,10 +1,36 @@
 Feature: HotSauceMall web browsing
   Scenario: Selecting product and adding to the cart
     Given User on the HotSauceMall website
-    When User clicks on any product and add it to the cart
+    When User clicks on any product
+    When User clicks on add to cart
     When User navigates to the Home page
-    When User will search for any product and add it to the cart
-    When User will go to the cart and proceed checkout
-    When User will fill all the required details for account creation and click on save
-    When User will go to the cart and complete the check out
-    When User will click on continue to ship and pay
+    When User will search a product and press enter
+    When User will click Randomly on one product
+    When User will click on add to cart button
+    When User will click on cart icon
+#    When Verify the no. of products in the cart
+    When User will click on checkout
+    When User will click on secure checkout
+    When User will click on create new account button
+    When User will fill all the required details for account creation
+    When User will click on save
+    When User will click on continue to shopping
+    When User will go to the cart
+    When User will click on checkout button
+    When User will click on secure checkout button
+    When User will click on continue to ship
+    When User will click on continue to pay
+
+
+#Launch "https://www.hotsaucemall.com/hot-sauce.html"
+#Randomly click on any of the products.
+#Click on Add to Cart
+#Go to Home Page
+#Search for a product
+#Select any one of the products randomly from the results returned from search.
+#Click on Add to Cart
+#In the Shopping Cart verify the no. of products added.
+#Click on Checkout
+#Click on Secure checkout
+#Create a user id
+#Continue with Next Steps.
--- tests/step_defs/test_hotsaucemall.py
+++ tests/step_defs/test_hotsaucemall.py
@@ -10,87 +10,110 @@ scenarios('../features/hotsaucemall.feature')
 
 
 @given("User on the HotSauceMall website")
-def test_homepage(browser):
+def home_page(browser):
     browser.get(url)
+    time.sleep(2)
 
 
-@when("User clicks on any product and add it to the cart")
-def test_click_and_add_product(browser):
+@when("User clicks on any product")
+def click_any_product(browser):
+    # randomly click on any product from homepage
     links = browser.find_elements_by_css_selector('a.mm-card-grid-item__text')
-    l = links[random.randint(0, len(links) - 1)]
-    l.click()
-    browser.find_element_by_css_selector(
-        'button.button.button--medium.bg--primary.color--black.font--bold.font--uppercase.h-nb').click()
-    # btn = browser.find_elements_by_css_selector('button.button.button--medium.bg--primary.color--black.font--bold.font--uppercase.h-nb')
-    # btn.click()
-    # list = browser.find_element_by_tag_name("a")
-    # l = list[random.Random(0, len(list))]
-    # l.click()
+    startint = 0
+    endint = 11
+    links[random.randint(startint,endint)].click()
+    time.sleep(2)
 
-    # browser.find_element_by_css_selector('a.mm-card-grid-item__text').click()
-    # browser.find_element_by_xpath('//*[@id="js-purchase-product"]/div[9]/button').click()
-    # Alert(browser).dismiss()
-    # action = ActionChains(browser)
-    # browser.find_element_by_xpath("/html/body/div[1]/main/div[4]/section/article/form/div[9]/button").click()
+
+@when("User clicks on add to cart")
+def click_add_to_cart(browser):
+    # Click on add to cart button
+    a = browser.find_element_by_css_selector('button.button.button--medium.bg--primary.color--black.font--bold.font--uppercase.h-nb')
+    a.click()
+    browser.find_element_by_xpath('//*[@id="js-PROD"]/div[2]/div/div[1]/div/button').click()
+    time.sleep(10)
 
 
 @when("User navigates to the Home page")
-def test_navigate_to_homepage(browser):
-    # browser.find_element_by_css_selector('.column whole.nav.ul li>a').click()
-    browser.find_element_by_css_selector('ul.breadcrumbs.font--medium.font--uppercase li').click()
-    # sys.stdout.flush()
-    # sleep(180)
-    # browser.find_element_by_xpath('//*[@id="js-site-wrapper"]/main/div[1]/section/nav/ul/li[1]/a')
+def navigate_to_homepage(browser):
+    # click on Home
+    b = browser.find_element_by_css_selector('ul.breadcrumbs.font--medium.font--uppercase li')
+    b.click()
+    time.sleep(2)
 
 
-@when("User will search for any product and add it to the cart")
-def test_search_add_product(browser):
+@when("User will search a product and press enter")
+def search_product(browser):
+    # click on search icon
     btn = browser.find_element_by_xpath('//*[@id="js-site-wrapper"]/header/div[2]/div/div[2]/button')
     btn.click()
-    # time.sleep(10)
-    # input_element = browser.find_element_by_xpath('//*[@id="l-search"]')
-    # input_element.clear()
-    # input_element = browser.find_element_by_css_selector("input[name^='Search']")
-    # input_element.clear()
-    # input_element.send_keys('PERI')
-    # input_element.send_keys(keys.ENTER)
-    # time.sleep(30)
+    time.sleep(2)
+    # pass value in search box
     browser.find_element_by_xpath('//*[@id="l-search"]').send_keys('PERI')
-    # time.sleep(10)
-    # ip.submit()
+    # press enter
     browser.find_element_by_xpath('//*[@id="l-search"]').send_keys(Keys.RETURN)
+    time.sleep(2)
+
+
+@when("User will click Randomly on one product")
+def random_click_product(browser):
     # selecting product randomly
     links = browser.find_elements_by_css_selector('a.mm-card-grid-item__text')
-    l = links[random.randint(0, len(links) - 1)]
-    l.click()
+    startint = 0
+    endint = 11
+    links[random.randint(startint,endint)].click()
+    time.sleep(2)
+
+
+@when("User will click on add to cart button")
+def add_to_cart(browser):
     # click add to cart
     browser.find_element_by_css_selector(
         'button.button.button--medium.bg--primary.color--black.font--bold.font--uppercase.h-nb').click()
-    time.sleep(60)
-    # time.sleep(10)
+    time.sleep(10)
 
 
-@when("User will go to the cart and proceed checkout")
-def test_cart_checkout(browser):
-    browser.find_element_by_xpath('//*[@id="js-PROD"]/div[2]/div/div[1]/div/button').click()
-    # time.sleep(60)
+@when("User will click on cart icon")
+def click_cart_icon(browser):
     # click on cart icon
     browser.find_element_by_xpath('//*[@id="js-site-wrapper"]/header/div[2]/div/div[5]/a').click()
+    time.sleep(2)
+
+
+# @when("Verify the no. of products in the cart")
+# def verify_products_in_cart(browser):
+#     elements = browser.find_elements_by_xpath('//*[@id="js-site-wrapper"]/header/div[2]/div/div[5]/a/span').text
+# #     elements = elements.text
+#     print(elements)
+#     time.sleep(2)
+
+
+@when("User will click on checkout")
+def click_checkout(browser):
     # click on check out button
     browser.find_element_by_xpath('//*[@id="js-site-wrapper"]/header/div[4]/div/div/div[7]/a').click()
-    # time.sleep(10)
+    time.sleep(2)
+
+
+@when("User will click on secure checkout")
+def click_secure_checkout(browser):
+    # scroll
     browser.execute_script("window.scrollBy(0, 350)")
     # secure check out button
-    # time.sleep(10)
     browser.find_element_by_css_selector(
         '.column.whole>a.button.button--block.button--large.bg--primary.color--black.font--bold.font--uppercase.h-nb').click()
-    # time.sleep(10)
+    time.sleep(2)
 
-
-@when("User will fill all the required details for account creation and click on save")
-def test_user_details(browser):
+@when("User will click on create new account button")
+def click_create_account(browser):
     browser.find_element_by_css_selector('a.button.bg--secondary.color--black.font--uppercase').click()
-    browser.find_element_by_css_selector('input#l-Customer_LoginEmail').send_keys('vv@gmail.com')
+    time.sleep(2)
+
+
+@when("User will fill all the required details for account creation")
+def user_details(browser):
+    #email
+    browser.find_element_by_css_selector('input#l-Customer_LoginEmail').send_keys('piii@gmail.com')
     browser.find_element_by_css_selector('input#l-Customer_Password').send_keys('xyz12345')
     browser.find_element_by_css_selector('input#l-Customer_VerifyPassword').send_keys('xyz12345')
     browser.find_element_by_css_selector('input#l-Customer_ShipFirstName').send_keys('xyz')
@@ -103,19 +126,11 @@ def test_user_details(browser):
     browser.find_element_by_css_selector('select#Customer_ShipCountry').send_keys("Canada")
     browser.find_element_by_css_selector('input#l-Customer_ShipState').send_keys("Alberta")
     browser.find_element_by_css_selector('input#l-Customer_ShipZip').send_keys('T9S')
-    # dropDownCountry.click()
-    # dropDownCountry.select_by_index(4)
-    # options = browser.find_elements_by_tag_name("option")
-    # for option in options:
-    #     option.click()
-    # country_field = browser.find_element_by_xpath('//*[@id="Customer_ShipCountry"]')
-    # country_field.send_keys("United States")
-    # sel= Select(browser.find_element_by_id("//*[@id="Customer_ShipCountry"]"))
-    # sel.select_by_visible_text('United States')
     browser.find_element_by_css_selector('input#l-Customer_BillFirstName').send_keys('xyz')
     browser.find_element_by_css_selector('input#l-Customer_BillLastName').send_keys('abc')
     browser.find_element_by_css_selector('input#l-Customer_BillCompany').send_keys('qwerty')
-    browser.find_element_by_css_selector('input#l-Customer_BillEmail').send_keys('vv@gmail.com')
+    # email
+    browser.find_element_by_css_selector('input#l-Customer_BillEmail').send_keys('piii@gmail.com')
     browser.find_element_by_css_selector('input#l-Customer_BillPhone').send_keys('123456754')
     browser.find_element_by_css_selector('input#l-Customer_BillAddress1').send_keys('mink1')
     browser.find_element_by_css_selector('input#l-Customer_BillAddress2').send_keys('mink2')
@@ -123,38 +138,61 @@ def test_user_details(browser):
     browser.find_element_by_css_selector('select#Customer_BillCountry').send_keys('Canada')
     browser.find_element_by_css_selector('input#l-Customer_BillState').send_keys('Alberta')
     browser.find_element_by_css_selector('input#l-Customer_BillZip').send_keys('T9S')
+    time.sleep(2)
+
+@when("User will click on save")
+def click_save(browser):
     browser.find_element_by_css_selector(
         'input.button.button--large.bg--primary.color--black.font--uppercase.h-nb').click()
-    # time.sleep(10)
+    time.sleep(2)
 
 
-@when('User will go to the cart and complete the check out')
-def test_cart_checkout(browser):
-    # click on continue the shopping
+@when("User will click on continue to shopping")
+def continue_to_Shopping(browser):
+# click on continue the shopping
     browser.find_element_by_xpath('//*[@id="js-site-wrapper"]/main/div[2]/p/a').click()
+    time.sleep(2)
+
+
+@when('User will go to the cart')
+def click_cart(browser):
     # click on cart icon
     browser.find_element_by_xpath('//*[@id="js-site-wrapper"]/header/div[2]/div/div[5]/a').click()
+    time.sleep(2)
+
+
+@when("User will click on checkout button")
+def click_checkout(browser):
     # click on check out button
     check = browser.find_element_by_css_selector(
         '.column.whole.medium-half.medium-offset-half.large-one-third.large-offset-two-thirds.global-mini-basket-drawer__wrap>div.row.bg--white>div.column.whole>a.button.button--block.button--large.button--square.bg--primary.color--black.font--bold.font--uppercase')
     check.click()
+    time.sleep(2)
+
+
+@when("User will click on secure checkout button")
+def click_secure_checkout(browser):
     # scroll
     browser.execute_script("window.scrollBy(0, 350)")
     # secure check out button
     browser.find_element_by_css_selector(
         '.column.whole>a.button.button--block.button--large.bg--primary.color--black.font--bold.font--uppercase.h-nb').click()
-    # time.sleep(1)
+    time.sleep(2)
 
 
-@when("User will click on continue to ship and pay")
-def test_ship_and_pay(browser):
+@when("User will click on continue to ship")
+def continue_to_ship(browser):
     # continue to ship
     browser.execute_script("window.scrollBy(0, 350)")
     browser.find_element_by_xpath(
         '//*[@id="js-site-wrapper"]/main/div[3]/section[2]/div[3]/div/ul/li/div/form/div[3]/div/input').click()
-    time.sleep(10)
-    # continue to payment
+    time.sleep(2)
+
+
+@when("User will click on continue to pay")
+def continue_to_pay(browser):
+    # continue to pay
     browser.execute_script("window.scrollBy(0, 350)")
     browser.find_element_by_xpath(
         '//*[@id="js-site-wrapper"]/main/div[3]/section[2]/div[3]/div/div[3]/div/button').click()
-    # time.sleep(10)
+    time.sleep(2)
